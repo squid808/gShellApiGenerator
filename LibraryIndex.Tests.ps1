57b33b81a19fad72f45438aed20d7eb6ae318d81
squid808 <squid808@gmail.com>
2018-03-06 16:45:39 -0500

    Working on some basic unit tests and trying to get them to work in Visual Studio Code. Gah.

---
+++ LibraryIndex.Tests.ps1
@@ -0,0 +1,77 @@
+﻿Describe "Format-Json" {
+
+    It 'errors on null input' {
+        {$null | Format-Json -ErrorAction Stop} | Should Throw "Cannot bind argument to parameter 'json' because it is an empty string."
+    }
+
+    It 'formats as expected' {
+        #region HereStrings
+        $stringin = @"
+{
+    "A":  1,
+    "B":  [
+{
+               "C":  2
+          },
+          {
+          "D": null
+   }
+      ]
+}
+"@
+
+        $stringout = @"
+{
+  "A": 1,
+  "B": [
+    {
+      "C": 2
+    },
+    {
+      "D": null
+    }
+  ]
+}
+
+"@
+        #endregion
+        $stringin | Format-Json | Should -BeExactly $stringout
+    }
+}
+
+Describe "Save-LibraryIndex" {
+
+    $TestPath = "TestDrive:\library.json"
+    $MockIndex = New-Object -Type PSObject
+    $MockIndex | Add-Member -MemberType NoteProperty -Name "RootPath" -Value $TestPath
+    $MockIndex | Add-Member -MemberType NoteProperty -Name "Libraries" -Value (New-Object psobject)
+
+    Context "Null Object" {
+        
+    }
+
+    Context "Null Root Path" {
+
+    }
+
+    Context "Working" {
+        Save-LibraryIndex -LibraryIndex $MockIndex
+        $result = Get-Content $MockIndex.RootPath -Raw
+
+        It "file exists" {
+            test-path $MockIndex.RootPath | Should -BeExactly $true
+        }
+
+        It "has content" {
+            $result | Should -BeExactly @"
+{
+  "RootPath": "TestDrive:\\library.json",
+  "Libraries": {
+  }
+}
+
+
+"@
+        }
+    }
+}
